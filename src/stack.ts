125316dba690f11f44d9495934f30e80511b8bca
Danial-Zaheer-Code <danialzaheer80@gmail.com>
2026-01-14 20:19:07 +0500

    Added incode docs for Stack.

--- src/stack.ts
+++ src/stack.ts
@@ -1,38 +1,70 @@
+/**
+ * A generic Stack implementation.
+ *
+ * Stores elements in LIFO (Last-In, First-Out) order.
+ *
+ * @typeParam T - Type of elements stored in the stack
+ */
 export class Stack<T> {
-	private items : T[] = [];
-	
-	peek(): T {
-		if(this.isEmpty()){
-			throw new Stack.EmptyStackError();
-		}
+	private items: T[] = [];
 
-		return this.items[this.items.length - 1]!;
-	}
- 
+	/**
+	 * Returns the number of elements in the stack.
+	 */
 	size(): number {
 		return this.items.length;
 	}
 
-	pop() : T {
-		if(this.isEmpty()){
+	/**
+	 * Checks whether the stack is empty.
+	 */
+	isEmpty(): boolean {
+		return this.size() === 0;
+	}
+
+	/**
+	 * Adds an element to the top of the stack.
+	 *
+	 * @param value - Element to push onto the stack
+	 */
+	push(value: T): void {
+		this.items.push(value);
+	}
+
+	/**
+	 * Removes and returns the top element of the stack.
+	 *
+	 * @throws EmptyStackError if the stack is empty
+	 */
+	pop(): T {
+		if (this.isEmpty()) {
 			throw new Stack.EmptyStackError();
 		}
 
 		return this.items.pop()!;
 	}
-	
-	push(value: T) {
-		this.items.push(value)
+
+	/**
+	 * Returns the element at the top of the stack without removing it.
+	 *
+	 * @throws EmptyStackError if the stack is empty
+	 */
+	peek(): T {
+		if (this.isEmpty()) {
+			throw new Stack.EmptyStackError();
+		}
+
+		return this.items[this.items.length - 1]!;
 	}
 
-	isEmpty(): boolean {
-		return this.size() === 0;
-	}
-
+	/**
+	 * Error thrown when attempting to access or remove
+	 * an element from an empty stack.
+	 */
 	static EmptyStackError = class extends Error {
-	constructor() {
-	  super("Stack is empty");
-	  this.name = "EmptyStackError";
-	}
-  };
+		constructor() {
+			super("Stack is empty");
+			this.name = "EmptyStackError";
+		}
+	};
 }
