4252dbf86ac7da28c4160b28c93b53cefc57c0d2
Danial-Zaheer-Code <danialzaheer80@gmail.com>
2026-01-22 10:19:53 +0500

    Added in-code DOCS for Deque.

--- src/Deque.ts
+++ src/Deque.ts
@@ -1,10 +1,28 @@
+
+/**
+ * A generic Double-Ended Queue implementation.
+ *
+ * It can store and remove elements from both ends.
+ *
+ * @typeParam T - Type of elements stored in the deque
+ */
 export class Deque<T> {
 	private items: T[] = [];
 
+	/**
+	 * Adds an element to the back of the deque.
+	 * 
+	 * @param value - Element to add
+	 */
 	pushBack(value: T): void {
 		this.items.push(value);
 	}
 
+	/**
+	 * Romoves and returns the value at the back of the deque.
+	 * 
+	 * @throws EmptyDequeError if the deque is empty.
+	 */
 	popBack(): T {
 		if (this.isEmpty()) {
 			throw new Deque.EmptyDequeError();
@@ -12,6 +30,11 @@ export class Deque<T> {
 		return this.items.pop()!;
 	}
 
+	/**
+	 * Returns the value at the back of the deque.
+	 * 
+	 * @throws EmptyDequeError if the deque is empty.
+	 */
 	peekBack(): T {
 		if (this.isEmpty()) {
 			throw new Deque.EmptyDequeError();
@@ -19,10 +42,20 @@ export class Deque<T> {
 		return this.items[this.items.length - 1]!;
 	}
 
+	/**
+	 * Adds an element to the front of the deque.
+	 * 
+	 * @param value - Element to add
+	 */
 	pushFront(value: T): void {
 		this.items.unshift(value);
 	}
 
+	/**
+	 * Romoves and returns the value at the front of the deque.
+	 * 
+	 * @throws EmptyDequeError if the deque is empty.
+	 */
 	popFront(): T {
 		if (this.isEmpty()) {
 			throw new Deque.EmptyDequeError();
@@ -30,6 +63,11 @@ export class Deque<T> {
 		return this.items.shift()!;
 	}
 
+	/**
+	 * Returns the value at the front of the deque.
+	 * 
+	 * @throws EmptyDequeError if the deque is empty.
+	 */
 	peekFront(): T {
 		if (this.isEmpty()) {
 			throw new Deque.EmptyDequeError();
@@ -37,18 +75,31 @@ export class Deque<T> {
 		return this.items[0]!;
 	}
 
+	/**
+	 * Checks whether the deque is empty.
+	 */
 	isEmpty(): boolean {
-		return this.items.length === 0;
+		return this.size() === 0;
 	}
 
+	/**
+	 * Returns the number of elements currently in the deque.
+	 */
 	size(): number {
 		return this.items.length;
 	}
 
+	/**
+	 * Clear all the elements from deque.
+	 */
 	clear(): void {
 		this.items = [];
 	}
 
+	/**
+	 * Error thrown when attempting to access or remove
+	 * an element from an empty deque.
+	 */
 	static EmptyDequeError = class extends Error {
 		constructor() {
 			super("Deque is empty");
